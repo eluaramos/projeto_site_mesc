6bff8c9c0d723fb1d6df00a0cba0ae64d687934b
João Victor <joaovfreitas00@gmail.com>
2026-07-06 10:53:39 -0300

    feat: adiciona banner de página interna à página de Contato

--- resources/js/pages/Contato.tsx
+++ resources/js/pages/Contato.tsx
@@ -9,6 +9,17 @@ const Contato: React.FC = () => {
       <Header />
 
       <main className="mx-0 my-0 p-0 bg-white">
+        {/* PAGE HEADER BANNER */}
+        <section className="page-header-banner">
+          <img src="/images/ict.jpg" alt="Instituto de Ciência e Tecnologia - UFF" />
+          <div className="page-header-overlay"></div>
+          <div className="container">
+            <h1 style={{ fontFamily: 'var(--font-serif)' }}>Contato</h1>
+            <div className="page-header-breadcrumb">
+              <a href="/">Home</a> <span className="mx-1">/</span> <span>Contato</span>
+            </div>
+          </div>
+        </section>
       </main>
 
       <Footer />
