0169bbd170dbff641eab2370fc444c673a23aa1d
LeoHazer <pecanha_leonardo@id.uff.br>
2026-08-10 17:25:06 -0300

    test: cria estrutura inicial de testes com Pest

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+test('página inicial carrega com sucesso', function () {
+    $response = $this->get('/');
+
+    $response->assertStatus(200);
+});
